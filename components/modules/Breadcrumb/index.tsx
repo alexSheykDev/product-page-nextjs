abfe55a60ca94af05e97d9be0db59c25be5757d0
oleksii.sheiko <alexxsheyk@gmail.com>
2025-03-04 00:12:15 +0200

    feat: added breadcrumb component

---
+++ components/modules/Breadcrumb/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+type BreadcrumbsProps = {
+  name: string;
+  category: string;
+};
+
+/* 
+  Not sue how it should work
+  For now simple display of the product category and name
+*/
+export default function Breadcrumbs({ name, category }: BreadcrumbsProps) {
+  return (
+    <nav className="text-sm">
+      <ul className="flex gap-2 text-headers">
+        <li className="opacity-40">{category}</li>
+        <li>{`/ ${name}`}</li>
+      </ul>
+    </nav>
+  );
+}
