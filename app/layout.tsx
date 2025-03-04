b244378847aa3e3240f866b78dbba9e1d42c02ce
oleksii.sheiko <alexxsheyk@gmail.com>
2025-03-04 10:20:18 +0200

    feat: added adobe font

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/iuq4mex.css" />
+      </head>
       <Head>
         <link
           rel="stylesheet"
